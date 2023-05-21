46d70a1eb2dc47ef8972cf58ff6945bebc2cda57
MochYusuf <70353463+jabjabrik@users.noreply.github.com>
2023-05-21 16:14:18 +0000

    add express

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/', (req, res) => {
-    res.status(200).json({ status: true, message: 'Hello world!!' });
+    res.status(200).json({ status: true, message: 'Hello world!! 2' });
 });
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
