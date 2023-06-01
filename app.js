959a6b97510fb862a99723ce291a30cd3008164a
MochYusuf <70353463+jabjabrik@users.noreply.github.com>
2023-06-01 06:35:31 +0000

    fix

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+console.log(process.env)
 
 app.use('/', (req, res) => {
     res.status(200).json({ status: true, message: 'Hello world!! 4' });
